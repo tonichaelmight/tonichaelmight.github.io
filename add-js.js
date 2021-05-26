78c1c7056a744fc99aa9e04ce5bfdc7515f119e9
tonichaelmight <tonichaelmight@gmail.com>
2021-05-26 18:08:03 -0400

    final touches

--- add-js.js
+++ add-js.js
@@ -2,7 +2,7 @@ const changeTitle = () => {
     document.getElementById('title').innerHTML = 'Pumpkin!';
 }
 
-setTimeout(changeTitle, 1000);
+setTimeout(changeTitle, 2000);
 
 const getRandNum = () => {
     return Math.floor(Math.random() * 256);
@@ -13,4 +13,4 @@ const changeColor = () => {
     document.getElementById('title').style.color = `rgb(${getRandNum()}, ${getRandNum()}, ${getRandNum()})`;
 }
 
-setTimeout(changeColor, 2000);
+setTimeout(changeColor, 4000);
