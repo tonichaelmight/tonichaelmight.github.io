8db8bd219ea899366a212c52401ac0e9fd3a23cf
tonichaelmight <tonichaelmight@gmail.com>
2021-06-17 04:03:22 -0400

    it's 4am lol but there's some good animation going on

--- education.js
+++ education.js
@@ -1,20 +1,59 @@
 const institutions = document.getElementsByClassName('institution');
 
-const clearInstitutions = () => {
+const resetInstitutions = () => {
     for (institution of institutions) {
         const expanded = institution.getElementsByClassName('expanded')[0];
-        console.log(expanded);
+        const arrow = institution.getElementsByClassName('arrow')[0];
+        arrow.style.borderLeft = '10px solid black';
+        arrow.style.transform = 'rotate(0deg)';
         expanded.hidden = true;
+        expanded.style.opacity = '0';
+        institution.style.height = institution.getElementsByClassName('always-up')[0].scrollHeight.toString() + 'px';
+    }
+}
+
+const highlightArrow = (event) => {
+    const institution = event.target.parentElement;
+    const expanded = institution.getElementsByClassName('expanded')[0];
+    const arrow = institution.getElementsByClassName('arrow')[0];
+    if (expanded.hidden) {
+        arrow.style.borderLeft = '10px solid white';
+    }
+}
+
+const unhighlightArrow = (event) => {
+    const institution = event.target.parentElement;
+    const expanded = institution.getElementsByClassName('expanded')[0];
+    const arrow = institution.getElementsByClassName('arrow')[0];
+    if (expanded.hidden) {
+        arrow.style.borderLeft = '10px solid black';
     }
 }
 
 const activateInstitution = (event) => {
-    clearInstitutions();
-    const expanded = event.target.parentElement.parentElement.getElementsByClassName('expanded')[0];
-    expanded.hidden = false;
+    const institution = event.target.parentElement;
+    const expanded = institution.getElementsByClassName('expanded')[0];
+    const arrow = institution.getElementsByClassName('arrow')[0];
+    
+    if (expanded.hidden) {
+        resetInstitutions();
+    
+        arrow.style.borderLeft = '10px solid white';
+        arrow.style.transform = 'rotate(90deg)';
+
+        institution.style.height = institution.scrollHeight.toString() + 'px';
+    
+        expanded.hidden = false;
+        expanded.style.opacity = '1';
+
+        institution.style.height = institution.scrollHeight.toString() + 'px';
+
+    }
 }
 
 for (institution of institutions) {
-    const arrow = institution.getElementsByClassName('arrow')[0];
-    arrow.addEventListener('click', activateInstitution, false);
+    const clickPanel = institution.getElementsByClassName('click-panel')[0];
+    clickPanel.addEventListener('mouseenter', highlightArrow, false);
+    clickPanel.addEventListener('mouseleave', unhighlightArrow, false)
+    clickPanel.addEventListener('click', activateInstitution, false);
 }
